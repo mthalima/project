8fec4dfc937da3f60603848e400aee4f90777cb1
mthalima <matheus.ecomp@gmail.com>
2024-11-22 01:28:10 -0300

    base css

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,14 @@ import { Get, Controller, Render } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  @Render('index')
+  @Render('form')
   root() {
     return { message: 'aleloia saiu o hello woeld com NESTJS!!' };
+  }
+
+  @Get('/form')
+  @Render('form') // 'form.hbs' or 'form.ejs', depending on your setup
+  getForm() {
+    return {};
   }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,hbs,ejs}', // Ajuste para o tipo de template usado no caso só estou usando hbs
+    './views/**/*.{html,hbs,ejs}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
